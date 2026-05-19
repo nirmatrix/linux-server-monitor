60fdff0472104c8e438ab07c8c20c753b78486a9
nirmatrix <pasidunirman2003@gmail.com>
2026-05-20 03:44:57 +0530

    Added Cron Automation

--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@ LOG_FILE="system.log"
 DISK_THRESHOLD=80
 RAM_THRESHOLD=70
 CPU_THRESHOLD=60
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
 get_disk_usage(){
 df -h / | awk 'NR==2 {gsub("%","",$5); print $5}'
@@ -15,7 +19,7 @@ get_cpu_usage(){
 vmstat 1 2 | tail -n 1 | awk '{print 100-$15}'
 }
 get_top_processes(){
-ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | grep -v "ps" | head -n 6
+ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6
 }
 
 check_status(){
@@ -25,12 +29,12 @@ THRESHOLD=$2
 
 if (( USAGE >= 95 ));
 then
-echo "[CRITICAL]"
+echo "${RED}[CRITICAL]${NC}"
 elif (( USAGE >= THRESHOLD ));
 then
-echo "[WARNING!]"
+echo "${YELLOW}[WARNING!]${NC}"
 else
-echo "[OK]"
+echo "${GREEN}[OK]${NC}"
 fi
 }
 
